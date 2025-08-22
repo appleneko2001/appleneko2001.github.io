8f3b989016eb0164a49af5bbac412da6dcc82375
appleneko2001 <appleneko2001@outlook.com>
2025-08-22 10:22:00 +0900

    add section is not ready component

---
+++ src/lib/components/profile/not-ready-placeholder.svelte
@@ -0,0 +1,15 @@
+<div class="page-not-ready">
+    <div><i>Section is not done yet</i></div>
+    <div><i>Blame the owner is too slow</i></div>
+</div>
+
+<style>
+    .page-not-ready {
+        width: 100vw;
+        height: 100vh;
+        font-size: 1.5em;
+
+        align-content: center;
+        text-align: center;
+    }
+</style>
