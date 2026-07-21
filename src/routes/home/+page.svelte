3efd52c04ce732d70aa3a4be0547ed58b579148f
appleneko2001 <appleneko2001@clewyarn.studio>
2026-07-21 20:36:42 +0900

    patch: trigger site clear cache in route "/home"

--- src/routes/home/+page.svelte
+++ src/routes/home/+page.svelte
@@ -2,6 +2,13 @@
     import { onMount } from "svelte";
 
     onMount(() => {
+
+      caches.keys().then((names) => {
+        for (let name of names) {
+          caches.delete(name);
+        }
+      });
+
       window.location.replace("/");
     });
 </script>
