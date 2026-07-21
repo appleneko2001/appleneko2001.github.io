9fab7d09586b86d1583055b4260a5b281dc0c9e0
appleneko2001 <appleneko2001@clewyarn.studio>
2026-07-21 20:19:41 +0900

    create name card component

---
+++ src/lib/components/name-card.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+    const {
+      name,
+      username
+    } = $props();
+
+</script>
+
+<div class="name-card">
+    <span class="name">{name}</span>
+    <span class="separator">・</span>
+    <span class="username">{username}</span>
+</div>
+
+<style>
+    .name-card {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        gap: 8px;
+    }
+
+    .name {
+        font-size: 2.4em;
+    }
+
+    .separator {
+        background-color: var(--foreground-colour);
+        width: 100%;
+        height: 2px;
+        color: transparent;
+    }
+</style>
